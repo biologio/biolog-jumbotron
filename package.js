7ac90f54f5e2e12d9120eada1aa0d979de83d374
Rashid Ahmad <decent10cs@gmail.com>
2015-10-10 14:51:54 +0800

    changes to README

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'decent10cs:telescope-jumbotron',
   summary: 'A configurable Jumbotron for Telescope.',
-  version: '0.1.1',
+  version: '0.1.1.1',
   git: 'https://github.com/biologio/biolog-jumbotron.git'
 });
 
